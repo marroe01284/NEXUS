076ac2d507ec1c734511f4931acfcd6ab843df2d
Tom Christer Schoon <142898062+TomChrister@users.noreply.github.com>
2024-03-27 22:50:12 +0100

    FEAT: added some js to the form

--- js/newuser.js
+++ js/newuser.js
@@ -1,13 +1,41 @@
 document.getElementById("loginForm"). addEventListener("submit", function (event) {
     event.preventDefault();
 
-    let firstName = document.getElementById("fname").value;
-    let lastName = document.getElementById("lname").value;
+    if (!this.checkValidity()) {
+        return;
+    }
+
+    let password = document.getElementById("pwd").value;
+    let confirmPassword = document.getElementById("pwd2").value;
+    if (password !== confirmPassword) {
+        alert("Passwords do not match!");
+        return;
+    }
+
+    let firstName = document.getElementById("firstName").value;
+    let lastName = document.getElementById("lastName").value;
     let username = document.getElementById("username").value;
 
-    localStorage.setItem("fname", firstName);
-    localStorage.setItem("lname", lastName);
+    localStorage.setItem("firstName", firstName);
+    localStorage.setItem("lastName", lastName);
     localStorage.setItem("username", username);
 
     window.location.href = "profile.html";
 });
+
+document.getElementById("pwd2").addEventListener("input", function () {
+    let password = document.getElementById("pwd").value;
+    let confirmPassword = this.value;
+    let messageElement = document.getElementById("passwordMatchMessage");
+
+    if (confirmPassword === '') {
+        messageElement.textContent = '';
+    } else if (password === confirmPassword) {
+        messageElement.textContent = "Passwords match!";
+        messageElement.style.color = "green";
+    } else {
+        messageElement.textContent = "Passwords do not match";
+        messageElement.style.color = "red";
+    }
+});
+
--- js/profile.js
+++ js/profile.js
@@ -1,3 +1,5 @@
-document.getElementById("fname").textContent = localStorage.getItem("fname");
-document.getElementById("lname").textContent = localStorage.getItem("lname");
+// this gets the info from registration page, you can use this when displaying
+// first and last name, and username on the profile page
+document.getElementById("firstName").textContent = localStorage.getItem("firstName");
+document.getElementById("lastName").textContent = localStorage.getItem("lastName");
 document.getElementById("username").textContent = localStorage.getItem("username");
